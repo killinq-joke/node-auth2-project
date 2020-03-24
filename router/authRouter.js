1f6e710fcc309328095e52eaf9b5849a2928fa12
Zak <zakadmaster@gmail.com>
2020-03-24 15:29:26 +0100

    Addition of token response

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,34 +1,50 @@
-const authRouter = require("express").Router()
-const bc = require("bcryptjs")
-const userModel = require("../models/userModel")
+const authRouter = require("express").Router();
+const bc = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
 authRouter.post("/register", (req, res) => {
-    const user = req.body
-    const hashPassword = bc.hashSync(user.password)
-    user.password = hashPassword
-    userModel.add(user)
+  const user = req.body;
+  const hashPassword = bc.hashSync(user.password);
+  user.password = hashPassword;
+  userModel
+    .add(user)
     .then(response => {
-        res.json(response)
+      res.json(response);
     })
     .catch(err => {
-        res.status(500).end()
-    })
-})
+      res.status(500).end();
+    });
+});
 
 authRouter.post("/login", (req, res) => {
-    const {username, password} = req.body
-    userModel.findBy({username})
+  const { username, password } = req.body;
+  userModel
+    .findBy({ username })
     .then(user => {
-        if(bc.compareSync(password, user.password)) {
-            res.json(user) 
-        } else {
-           res.status(401).end()
-        }
+      if (user && bc.compareSync(password, user.password)) {
+          const payload = {
+              sub: user.id,
+              username: user.username,
+              department: user.department
+          }
+          const options = {
+              expiresIn: 30 
+          }
+          
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            options);
+
+        res.json({token});
+      } else {
+        res.status(401).end();
+      }
     })
     .catch(err => {
-        res.status(500).end()
-    })
-})
+      res.status(500).end();
+    });
+});
 
-
-module.exports = authRouter
+module.exports = authRouter;
