dcdc464f40ef18bb1f3adbe56f4f39fd2cf79003
Zak <zakadmaster@gmail.com>
2020-03-24 13:53:30 +0100

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/db.sq3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+};
---
+++ router/userRouter.js
@@ -0,0 +1,5 @@
+const userRouter = require("express").Router()
+
+
+
+module.exports = userRouter
